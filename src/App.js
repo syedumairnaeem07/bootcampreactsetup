835893ad3201283499dc5e3500116262fc3b3b47
umair <sumairn.1997@gmail.com>
2020-08-23 16:35:50 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h3>Created By Umair</h3>
         </p>
         <a
           className="App-link"
